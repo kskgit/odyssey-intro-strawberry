661e6b4c7d87e41849d45e0b7acf06aaccbc1298
nakao keisuke <r3grashopper193@gmail.com>
2024-08-01 08:40:33 +0900

    schemaクラスの追加

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+from strawberry import schema
 from stawberry.fastapi import GraphQLRouter
 from fastapi import FastAPI
 
 app = FastAPI()
-graphql_router = GraphQLRouter(..., path="/", graphql_ide="appllo-sandbox")
+graphql_router = GraphQLRouter(schema, path="/", graphql_ide="appllo-sandbox")
 app.include_router(graphql_router)
