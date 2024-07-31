fbe9d41e67d024fd7087788343801fcd8b8d649f
nakao keisuke <r3grashopper193@gmail.com>
2024-08-01 08:38:24 +0900

    helloにresolverを追加

--- api/query.py
+++ api/query.py
@@ -4,4 +4,8 @@ from strawberry.types.base import StrawberryType
 
 @strawberry.type
 class Query:
-    hello: str
+    hello: str = strawberry.field(resolver=get_hello)
+
+
+def get_hello():
+    return "Hello world"
