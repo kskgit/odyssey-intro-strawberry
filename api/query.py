70afa2a96e1cb821a8704d959c1de5c8598f3e47
nakao keisuke <r3grashopper193@gmail.com>
2024-08-04 06:52:26 +0900

    featured_playlistsリゾルバを実装

--- api/query.py
+++ api/query.py
@@ -1,10 +1,13 @@
 import strawberry
+from .types.playlist import Playlist
 
 
-def get_hello():
-    return "Hello world"
-
-
-@strawberry.type
+@strawberry.type(description="Playlist hand-picked to be featured to all users.")
 class Query:
-    hello: str = strawberry.field(resolver=get_hello)
+    @strawberry.field
+    def featured_playlists(self) -> list[Playlist]:
+        return [
+            Playlist(id="1", name="GraphQL Groovin", description=None),
+            Playlist(id="2", name="GraphQL Explorer Jams", description=None),
+            Playlist(id="3", name="Interpretive GraphQL Dance", description=None),
+        ]
--- api/schema.py
+++ api/schema.py
@@ -1,6 +1,4 @@
 import strawberry
-from strawberry.federation import schema
 from .query import Query
-from .types.playlist import Playlist
 
-schema = strawberry.Schema(query=Query, types=[Playlist])
+schema = strawberry.Schema(query=Query)
