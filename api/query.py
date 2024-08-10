089c1cd584abe705fb21ff931033ab5a2c4c9c4a
nakao keisuke <r3grashopper193@gmail.com>
2024-08-10 17:02:43 +0900

    フィールドに説明の追加

--- api/query.py
+++ api/query.py
@@ -1,12 +1,11 @@
 import strawberry
-
 from .types.playlist import Playlist
 from mock_spotify_rest_api_client.api.playlists import get_featured_playlists
 
 
-@strawberry.type(description="Playlist hand-picked to be featured to all users.")
+@strawberry.type
 class Query:
-    @strawberry.field
+    @strawberry.field(description="Playlist hand-picked to be featured to all users.")
     async def featured_playlists(self, info: strawberry.Info) -> list[Playlist]:
         spotify_client = info.context["spotify_client"]
         data = await get_featured_playlists.asyncio(client=spotify_client)
