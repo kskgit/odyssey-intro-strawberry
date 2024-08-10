57a89008673b10edb579c0bc3f94095fc74f3755
nakao keisuke <r3grashopper193@gmail.com>
2024-08-10 16:57:42 +0900

    実際のspotifyのapiを叩けるようになる

--- api/query.py
+++ api/query.py
@@ -1,13 +1,20 @@
 import strawberry
+
 from .types.playlist import Playlist
+from mock_spotify_rest_api_client.api.playlists import get_featured_playlists
 
 
 @strawberry.type(description="Playlist hand-picked to be featured to all users.")
 class Query:
     @strawberry.field
-    def featured_playlists(self) -> list[Playlist]:
+    async def featured_playlists(self, info: strawberry.Info) -> list[Playlist]:
+        spotify_client = info.context["spotify_client"]
+        data = await get_featured_playlists.asyncio(client=spotify_client)
         return [
-            Playlist(id="1", name="GraphQL Groovin", description=None),
-            Playlist(id="2", name="GraphQL Explorer Jams", description=None),
-            Playlist(id="3", name="Interpretive GraphQL Dance", description=None),
+            Playlist(
+                id=strawberry.ID(playlist.id),
+                name=playlist.name,
+                description=playlist.description,
+            )
+            for playlist in data.playlists.items
         ]
