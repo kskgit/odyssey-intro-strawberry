efca8a18cd9fd901a09fdab538be456d4283c55d
nakao keisuke <r3grashopper193@gmail.com>
2024-08-14 08:53:15 +0900

    featured_playlistsクエリでtracksが取れるように型定義にリゾルバをついか

--- api/types/playlist.py
+++ api/types/playlist.py
@@ -1,6 +1,10 @@
 import strawberry
 
 from api.types.track import Track
+from mock_spotify_rest_api_client.api.playlists import get_playlists_tracks
+from mock_spotify_rest_api_client.models.spotify_object_paginated_spotify_object_playlist_track import (
+    SpotifyObjectPaginatedSpotifyObjectPlaylistTrack,
+)
 
 
 @strawberry.type(
@@ -16,5 +20,28 @@ class Playlist:
     _test: strawberry.Private[str] = None
 
     @strawberry.field(description="The tracks in the playlist.")
-    def tracks(self) -> list[Track]:
+    def tracks(self, info: strawberry.Info) -> list[Track] | None:
+        if self._tracks is None:
+            spotify_client = info.context["spotify_client"]
+
+            data = get_playlists_tracks.asyncio(
+                client=spotify_client, playlist_id=self.id
+            )
+
+            if data is None:
+                return None
+            if not isinstance(data, SpotifyObjectPaginatedSpotifyObjectPlaylistTrack):
+                return None
+
+            self._tracks = [
+                Track(
+                    id=strawberry.ID(item.track.id),
+                    name=item.track.name,
+                    duration_ms=item.track.duration_ms,
+                    explicit=item.track.explicit,
+                    uri=item.track.uri,
+                )
+                for item in data.items
+            ]
+
         return self._tracks
